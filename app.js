52d42ded4cc3122066d5bc327391a81142289b73
jaywomack <womackjw@hotmail.com>
2020-05-20 09:58:19 -0700

    Fixed links going to index.html from lessons.html problem ws preventdefault in app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ window.onscroll = function () {
 // Smooth Scrolling
 $("#navbar a, .btn").on("click", function (e) {
   if (this.hash !== "") {
-    e.preventDefault();
+    // e.preventDefault();
 
     const hash = this.hash;
 
